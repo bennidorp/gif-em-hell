048abe8ab76832a02af279a9412af4a8d6d4af84
benni dorp <benni.dorp@gmail.com>
2021-04-01 09:28:44 +0200

    done... fast

--- public/script.js
+++ public/script.js
@@ -6,20 +6,22 @@ const gifInput = document.getElementById("gif-search");
 const searchButton = document.getElementById("search-button");
 const results = document.getElementById("results");
 
-const name = prompt("Please enter your name");
-appendMessage("You joined the chat");
-socket.emit("new-user", name);
+setTimeout(() => {
+    const name = prompt("Please enter your name");
+    appendMessage("You joined the chat");
+    socket.emit("new-user", name);
+}, 1000);
 
-socket.on("chat-message", (data) => {
-    appendMessage(`${data.name}: ${data.message}`);
-});
+// socket.on("chat-message", (data) => {
+//     appendMessage(`${data.name}: ${data.message}`);
+// });
 
 socket.on("user-connected", (name) => {
     appendMessage(`${name} joined the Chat`);
 });
 
 socket.on("user-disconnected", (name) => {
-    appendMessage(`${name} left`);
+    appendMessage(`${name} left the Chat`);
 });
 
 messageForm.addEventListener("submit", (event) => {
@@ -37,13 +39,15 @@ function appendMessage(message) {
 }
 
 socket.on("gif-to-chat", (gif) => {
-    console.log("gif", gif);
+    appendMessage(gif.name);
+    //console.log("gif", gif);
     appendGif(gif.gif);
 });
 function appendGif(gif) {
     const imgElement = document.createElement("img");
     imgElement.src = gif;
     messageBody.append(imgElement);
+    messageBody.scrollTop = 1000000;
 }
 
 searchButton.addEventListener("click", async () => {
@@ -53,14 +57,15 @@ searchButton.addEventListener("click", async () => {
         params: {
             api_key: "WGFcilDHkLL3f2vgs3esFciDvoyJDIIh",
             q: gifInput.value,
-            limit: 3,
+            limit: 4,
             //offset : this.state.offset
         },
     });
     console.log("res", res.data);
+    results.style.visibility = "visible";
     results.innerHTML = "";
     res.data.data.forEach((gif) => {
-        results.innerHTML += `<img src="${gif.images.preview_gif.url}"/>`;
+        results.innerHTML += `<img src="${gif.images.fixed_height.url}"/>`;
     });
 });
 
